0abc6dc9707c922b779745631edc3192440d67bb
samsungsql <118648082+samsungsql@users.noreply.github.com>
2022-11-30 22:33:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel © Powered by : https://t.me/TinsonTs</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "JOIN @TinsonTs For more Files\n\n<b>{filename} SUPPORT US</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "FILE READY {filename}\n\n<b>JOIN @TinsonTs FOR MORE MOVIES</b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
